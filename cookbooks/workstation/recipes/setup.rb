597caf80c5204ff817bebbd4530c3ffb42f7ab7a
Srinivasa Nagaraja <sri.lynx@gmail.com>
2017-03-18 14:52:57 +0000

    setup.rb File for package management created inside cookbooks/workstation/recipes

---
+++ cookbooks/workstation/recipes/setup.rb
@@ -0,0 +1,17 @@
+##Setup.rb File for installation of Packages 
+#
+package 'ntp' do
+
+  action :install
+end
+
+package 'git' do
+ 
+ action :install
+end
+
+package 'tree' do
+
+ action :install
+end
+
