0c23936f291c2e55fcbc7d888247de3997eb2abc
Srinivasa Nagaraja <sri.lynx@gmail.com>
2017-03-18 14:41:14 +0000

    Sample2.rb File has been added with Comment

--- sample2.rb
+++ sample2.rb
@@ -2,7 +2,7 @@ file '/etc/motd' do
 
 action :create 
 
-content 'This is the Property of UTTARA INFO SYSTEMS .. Beware and Use If it\'s Intended Purposes'
+content "This is the Property of UTTARA INFO SYSTEMS .. Beware and Use If it's Intended Purposes\n"
 
 end
 
